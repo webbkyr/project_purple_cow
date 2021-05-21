c2a438f24729228ec812b03851e3f1f2039c8375
Kayla Webb <kaylarwebb@gmail.com>
2021-05-20 20:28:07 -0400

    feat: create the main counter component

---
+++ src/components/Counter.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <span>{{ message }}</span>
+    <custom-button text="Increment me!" @clickHandler="makeCountRequest" />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import CustomButton from "./CustomButton.vue";
+import api, { requestUrl } from "@/api";
+
+@Component({
+  components: {
+    CustomButton
+  }
+})
+export default class Counter extends Vue {
+  public count = 0;
+
+  async created() {
+    this.makeCountRequest();
+  }
+
+  get message() {
+    const isSingleView = this.count === 1;
+    return `
+      This counter has been clicked ${this.count} 
+      ${isSingleView ? "time." : "times."}
+    `;
+  }
+
+  async makeCountRequest(): Promise<void> {
+    const response = (await api.get(requestUrl)).data;
+    this.count = response.value;
+  }
+}
+</script>
+
+<style scoped></style>
