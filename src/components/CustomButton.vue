a57e7fde4029951f9db600dcd2fea356b9e95b20
Kayla Webb <kaylarwebb@gmail.com>
2021-05-20 20:23:31 -0400

    feat: create reusable button component

---
+++ src/components/CustomButton.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <button @click="handleClick">{{ text }}</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class Button extends Vue {
+  @Prop() text!: string;
+
+  handleClick($event: MouseEvent) {
+    this.$emit("clickHandler", $event);
+  }
+}
+</script>
+
+<style scoped></style>
